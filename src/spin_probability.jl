1e652c24c7a921f0feaff8ff3081002a08b67f82
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-10-10 21:56:49 +0530

    updates

--- src/spin_probability.jl
+++ src/spin_probability.jl
@@ -6,7 +6,7 @@ function bloch!(du, u, p, t)
     du[4] = 0.5 * (p[2] * u[3] - p[3] * cos(p[1]*t) * u[1])
 end
 
-function prob_down(func, u0, tspan, p)
+function spin_down_probability(func, u0, tspan, p)
     problem = ODEProblem(func,u0,tspan,p)
     sol = solve(problem,RK4())
     prob = sol[3,:].^2 + sol[4,:].^2
@@ -14,19 +14,10 @@ function prob_down(func, u0, tspan, p)
 end
 
 #%%
-u0 = [1.0;0;0;0]
-tspan = (0.0,200.0)
-p = [1.0;1.0;0.1]
-
-plt.title("Probability of spin down")
-plt.plot(prob_down(bloch!, u0, tspan, p)[1], prob_down(bloch!, u0, tspan, p)[2])
-plt.xlabel("t (ns)")
-plt.ylabel("Probability of spin down")
-
-#%%
 ## Plotting flipping probability
 N = 1000
 ω0 = 1.0; ω1 = 0.1
+u0 = [1.0, 0, 0, 0]
 tmax = 2.1 * pi / ω1
 tspan = (0.0,tmax)
 omega_array = range(0.9, 1.1, length=N)
@@ -35,7 +26,7 @@ prob_array1 = zeros(N)
 for i = 1:N
     ω = omega_array[i]
     p = [ω, ω0, ω1]
-    p_b = prob_down(bloch!, u0, tspan, p)[2]
+    p_b = spin_down_probability(bloch!, u0, tspan, p)[2]
     prob_array1[i] = maximum(p_b)
 end
 
@@ -51,7 +42,7 @@ prob_array2 = zeros(N)
 for i = 1:N
     ω = omega_array[i]
     param = [ω, ω0, ω1]
-    p_b = prob_down(bloch!, u0, tspan, param)[2]
+    p_b = spin_down_probability(bloch!, u0, tspan, param)[2]
     prob_array2[i] = maximum(p_b)
 end
 
@@ -67,8 +58,8 @@ plt.legend()
 using FFTW
 
 function eval_power_spectrum(u0, tspan, param)
-    p_b = prob_down(bloch!, u0, tspan, param)[2]
-    t_array = prob_down(bloch!, u0, tspan, param)[1]
+    p_b = spin_down_probability(bloch!, u0, tspan, param)[2]
+    t_array = spin_down_probability(bloch!, u0, tspan, param)[1]
     fft_pb = fft(p_b)
     power_spec = broadcast(abs, fft_pb).^2
     power_spec[1] = 0
